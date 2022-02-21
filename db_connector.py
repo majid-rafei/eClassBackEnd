23391259cb1f5158827c6a3b457296a4df5650b2
StoyanStAtanasov <s.atanasov@dundts.com>
2022-02-21 17:38:13 +0100

    fix(db access): Does not find driver

--- db_connector.py
+++ db_connector.py
@@ -4,23 +4,25 @@ from singleton import Singleton
 
 
 class DbConnection(metaclass=Singleton):
-	"""
-	Is the singleton class for creating and maintaining database connection
-	"""
-	def __init__(self):
-		driver = 'SQLITE3'
-		server = 'localhost'
-		database = settings.DATABASES['default']['NAME']
-		print('Connecting to the database: ', database, '...')
-		self.dbconn = pyodbc.connect("DRIVER={};".format(driver) + \
-									 "SERVER={};".format(server) + \
-									 "DATABASE={};".format(database) + \
-									 'Trusted_Connection=yes;' + \
-									 "CHARSET=UTF8",
-									 ansi=True)
+    """
+    Is the singleton class for creating and maintaining database connection
+    """
 
-	def get_cursor(self):
-		return self.dbconn.cursor()
+    def __init__(self):
+        # Installed Sqlite odbc driver for windows http://www.ch-werner.de/sqliteodbc/sqliteodbc_w64.exe
+        driver = 'SQLite3 ODBC Driver'
+        server = 'localhost'
+        database = settings.DATABASES['default']['NAME']
+        print('Connecting to the database: ', database, '...')
+        self.dbconn = pyodbc.connect("DRIVER={};".format(driver) +
+                                     "SERVER={};".format(server) +
+                                     "DATABASE={};".format(database) +
+                                     'Trusted_Connection=yes;' +
+                                     "CHARSET=UTF8",
+                                     ansi=True)
 
-	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.dbconn.close()
+    def get_cursor(self):
+        return self.dbconn.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.dbconn.close()
